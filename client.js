5ce9c6e591f05dff5e016e8d3d113e64a24439b6
HishamAlmoli <halmoli@my.centennialcollege.ca>
2022-11-23 22:27:00 -0500

    Exporting connection from client.js file is done

---
+++ client.js
@@ -0,0 +1,31 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+    const conn = net.createConnection({
+      host: '165.227.47.243',// IP address here,
+      port: 50541 // PORT number here,
+    });
+  
+    // interpret incoming data as text
+    conn.setEncoding("utf8");
+  
+    conn.on('data', (data) => {
+      console.log(data);
+  });
+  
+  // process.stdin.on('data', (userInput) => {
+  //     client.write(userInput);
+  // });
+  
+    return conn;
+  };
+  
+  
+  /*
+  Here’s the info for the Snek server!
+  View the snakes : http://165.227.47.243:8000/view
+  Host: 165.227.47.243
+  Port: 50541
+  */
+  module.exports = { connect };
